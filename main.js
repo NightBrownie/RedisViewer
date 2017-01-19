d48c54ed405d899684f03e124df10ad5affc4dd2
Nikita Klyushnikov <vipkonst@gmail.com>
2017-01-19 15:35:57 +0300

    Updated dependencies, refactored main.js code.

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const electron = require('electron');
 
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const { app, BrowserWindow } = electron;
 
 const path = require('path');
 const url = require('url');
