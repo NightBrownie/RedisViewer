e4616348e86f8c85f625700bbd1adfab23fee456
Nikita Klyushnikov <vipkonst@gmail.com>
2017-04-25 15:14:28 +0300

    Refactored main container and main component.

---
+++ app/components/main.js
@@ -0,0 +1,34 @@
+import React, {Component, PropTypes} from 'react';
+
+import SplitPane from 'react-split-pane';
+
+export default class Main extends Component {
+    static propTypes = {
+        treeViewWidth: PropTypes.number,
+        treeViewWidthChanged: PropTypes.func.isRequired,
+        treeViewWidthRestoreRequested: PropTypes.func.isRequired
+    };
+
+    componentWillMount() {
+        this.props.treeViewWidthRestoreRequested();
+    }
+
+    render() {
+        return (
+            <SplitPane
+                className="main-layout-container"
+                split="vertical"
+                minSize={150}
+                defaultSize={this.props.treeViewWidth}
+                onChange={this.props.treeViewWidthChanged}
+                maxSize={-150}
+            >
+                <div className="left-panel">
+                </div>
+                <div className="right-panel">
+                    {/*TODO: routing here*/}
+                </div>
+            </SplitPane>
+        );
+    }
+}
--- app/containers/main.js
+++ app/containers/main.js
@@ -1,41 +1,8 @@
-import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
+import Main from "../components/main";
 import * as configActions from '../actions/configActions';
-
-import SplitPane from 'react-split-pane';
-
-export class Main extends Component {
-    static propTypes = {
-        treeViewWidth: PropTypes.number,
-        treeViewWidthChanged: PropTypes.func.isRequired,
-        treeViewWidthRestoreRequested: PropTypes.func.isRequired
-    };
-
-    componentWillMount() {
-        this.props.treeViewWidthRestoreRequested();
-    }
-
-    render() {
-        return (
-            <SplitPane
-                className="main-layout-container"
-                split="vertical"
-                minSize={150}
-                defaultSize={this.props.treeViewWidth}
-                onChange={this.props.treeViewWidthChanged}
-                maxSize={-150}
-            >
-                <div className="left-panel">
-                </div>
-                <div className="right-panel">
-                    {/*TODO: routing here*/}
-                </div>
-            </SplitPane>
-        );
-    }
-}
 
 //Decorator doesn't seem to work with react-hot-loader and react-transform-hmr yet... :(
 export default connect(
