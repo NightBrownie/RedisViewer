abef956fc6715cbe465d11c9be4c4c488269f5eb
Nikita Klyushnikov <vipkonst@gmail.com>
2017-05-11 18:21:01 +0300

    Added ioredis package as main redis library. Added node-keytar for safer configs. Added auto-hide behavior to menu bar.

--- app/main.development.js
+++ app/main.development.js
@@ -1,69 +1,71 @@
-import { app, BrowserWindow } from 'electron';
+import {app, BrowserWindow} from 'electron';
 import MenuBuilder from './menu';
 
 
 let mainWindow = null;
 
 if (process.env.NODE_ENV === 'production') {
-  const sourceMapSupport = require('source-map-support');
-  sourceMapSupport.install();
+    const sourceMapSupport = require('source-map-support');
+    sourceMapSupport.install();
 }
 
 if (process.env.NODE_ENV === 'development') {
-  require('electron-debug')();
-  const path = require('path');
-  const p = path.join(__dirname, '..', 'app', 'node_modules');
-  require('module').globalPaths.push(p);
+    require('electron-debug')();
+    const path = require('path');
+    const p = path.join(__dirname, '..', 'app', 'node_modules');
+    require('module').globalPaths.push(p);
 }
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+    if (process.platform !== 'darwin') app.quit();
 });
 
 
-const installExtensions = async () => {
-  if (process.env.NODE_ENV === 'development') {
-    const installer = require('electron-devtools-installer');
+const installExtensions = async() => {
+    if (process.env.NODE_ENV === 'development') {
+        const installer = require('electron-devtools-installer');
 
-    const extensions = [
-      'REACT_DEVELOPER_TOOLS',
-      'REDUX_DEVTOOLS'
-    ];
+        const extensions = [
+            'REACT_DEVELOPER_TOOLS',
+            'REDUX_DEVTOOLS'
+        ];
 
-    const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
+        const forceDownload = !!process.env.UPGRADE_EXTENSIONS;
 
-    // TODO: Use async interation statement.
-    //       Waiting on https://github.com/tc39/proposal-async-iteration
-    //       Promises will fail silently, which isn't what we want in development
-    return Promise
-      .all(extensions.map(name => installer.default(installer[name], forceDownload)))
-      .catch(console.log);
-  }
+        // TODO: Use async interation statement.
+        //       Waiting on https://github.com/tc39/proposal-async-iteration
+        //       Promises will fail silently, which isn't what we want in development
+        return Promise
+            .all(extensions.map(name => installer.default(installer[name], forceDownload)))
+            .catch(console.log);
+    }
 };
 
-app.on('ready', async () => {
-  await installExtensions();
+app.on('ready', async() => {
+    await installExtensions();
 
-  mainWindow = new BrowserWindow({
-    show: false,
-    width: 1024,
-    height: 728
-  });
+    mainWindow = new BrowserWindow({
+        show: false,
+        width: 1024,
+        height: 728
+    });
 
-  mainWindow.loadURL(`file://${__dirname}/app.html`);
+    mainWindow.loadURL(`file://${__dirname}/app.html`);
 
-  mainWindow.webContents.on('did-finish-load', () => {
-    if (!mainWindow) {
-      throw new Error('"mainWindow" is not defined');
-    }
-    mainWindow.show();
-    mainWindow.focus();
-  });
+    mainWindow.setAutoHideMenuBar(true);
 
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+    mainWindow.webContents.on('did-finish-load', () => {
+        if (!mainWindow) {
+            throw new Error('"mainWindow" is not defined');
+        }
+        mainWindow.show();
+        mainWindow.focus();
+    });
 
-  const menuBuilder = new MenuBuilder(mainWindow);
-  menuBuilder.buildMenu();
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+
+    const menuBuilder = new MenuBuilder(mainWindow);
+    menuBuilder.buildMenu();
 });
