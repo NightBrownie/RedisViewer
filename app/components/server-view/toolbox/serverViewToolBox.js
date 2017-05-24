89aee93fae8e61ceb434e1a035b1fb7494e8ff9f
Nikita Klyushnikov <vipkonst@gmail.com>
2017-05-24 17:08:43 +0300

    Updated toolbox styling, adde FontAwesome icons.

--- app/components/server-view/toolbox/serverViewToolBox.js
+++ app/components/server-view/toolbox/serverViewToolBox.js
@@ -6,9 +6,15 @@ export default class ServerViewToolBox extends Component {
     render() {
         return (<div className="server-view-toolbox">
           <ServerViewSearchTool />
-          <button className="toolbox-btn add-server-btn add-server-icon" />
-          <button className="toolbox-btn settings-btn settings-icon" />
-          <button className="toolbox-btn remove-btn remove-icon" />
+            <button className="toolbox-btn add-server-btn">
+                <span className="add-server-icon fa fa-server fa-fw fa-lg" />
+            </button>
+            <button className="toolbox-btn settings-btn">
+                <span className="settings-icon fa fa-cog fa-fw fa-lg" />
+            </button>
+            <button className="toolbox-btn remove-btn">
+                <span className="remove-icon fa fa-trash-o fa-fw fa-lg" />
+            </button>
         </div>);
     }
 }
--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ import { AppContainer } from 'react-hot-loader';
 import { syncHistoryWithStore } from 'react-router-redux';
 import Root from './containers/root';
 import configureStore from './store/configureStore';
+import 'font-awesome/less/font-awesome.less';
 import './styles/main.less';
 
 const store = configureStore();
