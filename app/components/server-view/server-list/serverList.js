e35482201766bfe46caa241f704d7dbeadb32e75
NightBrownie <me@nikitaklyushnikov.com>
2017-05-28 01:36:47 +0300

    Updated styling for server view list offset spans. Set up standardjs code linting.

--- app/components/server-view/server-list/serverList.js
+++ app/components/server-view/server-list/serverList.js
@@ -1,44 +1,100 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react'
 
-import ServerListItem from './serverListItem';
+import ServerListItem from './serverListItem'
 
-import * as treeViewSpanTypes from '../../../constants/treeViewSpanTypes';
-import * as treeViewItemTypes from '../../../constants/treeViewItemTypes';
+import * as treeViewSpanTypes from '../../../constants/treeViewSpanTypes'
+import * as treeViewItemTypes from '../../../constants/treeViewItemTypes'
 
 export default class ServerList extends Component {
-    render() {
-        return (<div className="server-list-container">
-          <ul className="server-list">
-            <ServerListItem
-              treeViewSpans={[treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN]}
-              isExpandable
-              isExpanded={false}
-              itemType={treeViewItemTypes.TREE_VIEW_FOLDER_ITEM}
-              name={'Test folder name'}
-            />
-            <ServerListItem
-              treeViewSpans={[treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN]}
-              isExpandable
-              isExpanded
-              itemType={treeViewItemTypes.TREE_VIEW_FOLDER_ITEM}
-              name={'Test folder name'}
-            />
-            <ServerListItem
-              treeViewSpans={[treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN]}
-              itemType={treeViewItemTypes.TREE_VIEW_SERVER_ITEM}
-              name={'Test folder name'}
-            />
-            <ServerListItem
-              treeViewSpans={[treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN]}
-              itemType={treeViewItemTypes.TREE_VIEW_DATABASE_ITEM}
-              name={'Test folder name'}
-            />
-            <ServerListItem
-              treeViewSpans={[treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN]}
-              itemType={treeViewItemTypes.TREE_VIEW_KEY_ITEM}
-              name={'Test folder name'}
-            />
-          </ul>
-        </div>);
-    }
+  render () {
+    return (<div className='server-list-container'>
+      <ul className='server-list'>
+        <ServerListItem
+          treeViewSpans={[treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN]}
+          isExpandable
+          isExpanded
+          itemType={treeViewItemTypes.TREE_VIEW_FOLDER_ITEM}
+          name={'Test folder name'}
+                />
+        <ServerListItem
+          treeViewSpans={[treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_NODE_SPAN]}
+          itemType={treeViewItemTypes.TREE_VIEW_SERVER_ITEM}
+          isExpanded={false}
+          name={'Test folder name'}
+                />
+        <ServerListItem
+          treeViewSpans={[treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_LAST_NODE_SPAN]}
+          itemType={treeViewItemTypes.TREE_VIEW_SERVER_ITEM}
+          isExpanded
+          name={'Test folder name'}
+          />
+        <ServerListItem
+          treeViewSpans={[treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_NODE_SPAN]}
+          itemType={treeViewItemTypes.TREE_VIEW_DATABASE_ITEM}
+          isExpanded
+          name={'Test folder name'}
+                />
+        <ServerListItem
+          treeViewSpans={[treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_NODELESS_SPAN,
+            treeViewSpanTypes.TREE_VIEW_NODE_SPAN]}
+          itemType={treeViewItemTypes.TREE_VIEW_KEY_ITEM}
+          name={'Test folder name'}
+                />
+        <ServerListItem
+          treeViewSpans={[treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_NODELESS_SPAN,
+            treeViewSpanTypes.TREE_VIEW_NODE_SPAN]}
+          itemType={treeViewItemTypes.TREE_VIEW_KEY_ITEM}
+          name={'Test folder name'}
+          />
+        <ServerListItem
+          treeViewSpans={[treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_NODELESS_SPAN,
+            treeViewSpanTypes.TREE_VIEW_NODE_SPAN]}
+          itemType={treeViewItemTypes.TREE_VIEW_KEY_ITEM}
+          name={'Test folder name'}
+          />
+        <ServerListItem
+          treeViewSpans={[treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_NODELESS_SPAN,
+            treeViewSpanTypes.TREE_VIEW_NODE_SPAN]}
+          itemType={treeViewItemTypes.TREE_VIEW_KEY_ITEM}
+          name={'Test folder name'}
+          />
+        <ServerListItem
+          treeViewSpans={[treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_NODELESS_SPAN,
+            treeViewSpanTypes.TREE_VIEW_LAST_NODE_SPAN]}
+          itemType={treeViewItemTypes.TREE_VIEW_KEY_ITEM}
+          name={'Test folder name'}
+          />
+        <ServerListItem
+          treeViewSpans={[treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_LAST_NODE_SPAN]}
+          itemType={treeViewItemTypes.TREE_VIEW_DATABASE_ITEM}
+          isExpanded
+          name={'Test folder name'}
+          />
+        <ServerListItem
+          treeViewSpans={[treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_EMPTY_SPAN,
+            treeViewSpanTypes.TREE_VIEW_LAST_NODE_SPAN]}
+          itemType={treeViewItemTypes.TREE_VIEW_KEY_ITEM}
+          name={'Test folder name'}
+          />
+      </ul>
+    </div>)
+  }
 }
