334fa6c9a8b527f481ce7f49abac9bc95a36172a
NightBrownie <me@nikitaklyushnikov.com>
2017-05-29 23:14:40 +0300

    Added form and buttons into server settings view.

--- app/components/server-settings/serverSettings.js
+++ app/components/server-settings/serverSettings.js
@@ -1,16 +1,37 @@
 import React, {Component} from 'react'
 
-import PrimaryServerSettings from './primaryServerSettings';
-import AdvancedServerSettings from './advancedServerSettings';
-import Expander from '../expander';
+import PrimaryServerSettings from './primaryServerSettings'
+import AdvancedServerSettings from './advancedServerSettings'
+import Expander from '../expander'
+import Button from '../button'
 
 export default class ServerSettings extends Component {
   render () {
-    return (<div className="server-settings">
+    return (<div className='server-settings'>
+      <h2 className='server-settings-form-label'>
+          Add new server
+      </h2>
+      <form>
         <PrimaryServerSettings />
         <Expander>
-            <AdvancedServerSettings />
+          <AdvancedServerSettings />
         </Expander>
-    </div>);
+
+        <div className='buttons-container'>
+          <Button className='test-connection-button'>
+            <i
+              className='test-connection-button-icon fa fa-refresh fa-fw fa-lg'
+              aria-hidden='true'
+            />
+          </Button>
+          <Button className='save-server-settings-button'>
+              Save
+          </Button>
+          <Button className='cancel-server-settings-changes-button'>
+              Cancel
+          </Button>
+        </div>
+      </form>
+    </div>)
   }
 }
--- app/components/server-view/toolbox/serverViewSearchTool.js
+++ app/components/server-view/toolbox/serverViewSearchTool.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import LabeledInput from '../../labeledInput';
+import LabeledInput from '../../labeledInput'
 
 export default class ServerViewSearchTool extends Component {
-    render() {
-        return (<LabeledInput
-            className="server-view-search-tool"
-            placeholder="Type filter term here..."
-            type="text"
-            title="Input search term here"
+  render () {
+    return (<LabeledInput
+      className='server-view-search-tool'
+      placeholder='Type filter term...'
+      type='text'
+      title='Input search term here'
         >
-            <i className="search-icon fa fa-search fa-fw" aria-hidden="true" />
-        </LabeledInput>);
-    }
+      <i className='search-icon fa fa-search fa-fw' aria-hidden='true' />
+    </LabeledInput>)
+  }
 }
